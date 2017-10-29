c9b0cea36d6772d17f84428b5c97d8bad4a71f11
Peter Standbridge <peterstandbridge@Peters-MacBook-Air.local>
2017-10-29 17:28:16 -0400

    consolidate code to base on SELF

--- ps_pythonGettingStarted/classes.py
+++ ps_pythonGettingStarted/classes.py
@@ -5,11 +5,15 @@ students = []
 
 class Student:
     def __init__(self, name, student_id=332):  # initialiization
-        student = {"name": name, "student_id": student_id}
-        students.append(student)
+        self.name = name
+        self.student_id = student_id
+        students.append(self)
 
     def __str__(self):
-        return "Student"
+        return "Student " + self.name
+
+    def get_name_capitalize(self):
+        return self.name.capitalize()
 
 mark = Student("Mark")
 print(mark)
