9a5779d26819ab3bfadc5577433bf0085a4eb572
Peter Standbridge <peterstandbridge@Peters-MacBook-Air.local>
2017-10-29 17:36:24 -0400

    added school variable

--- ps_pythonGettingStarted/classes.py
+++ ps_pythonGettingStarted/classes.py
@@ -4,6 +4,11 @@
 students = []
 
 class Student:
+
+    # adding static variable (class attribute) ~ as opposed to instance attributes
+    school_name = "Central Elementary"
+
+# function adds self [instance attribute] value to "students"
     def __init__(self, name, student_id=332):  # initialiization
         self.name = name
         self.student_id = student_id
@@ -15,7 +20,13 @@ class Student:
     def get_name_capitalize(self):
         return self.name.capitalize()
 
-mark = Student("Mark")
-print(mark)
+    # create a function that returns school name
+    def get_school_name(self):
+        return self.school_name
+
+print(Student.school_name)
+
+#mark = Student("Mark")
+#print(mark)
 
 #print(students)
